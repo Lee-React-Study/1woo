3c47a02a7c813a2d335f1cae2670a0545ca860c6
choi-il-woo <chldlfdn0440@naver.com>
2025-10-02 09:23:31 +0900

    공부

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,33 +1,53 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  };
-
   return (
     <>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/new">New</Link>
-        <Link to="/diary">Diary</Link>
-      </div>
-      <button onClick={onClickButton}>new 페이지로 이동</button>
+      <Header
+        title={"Header"}
+        leftChild={<Button text="left" />}
+        rightChild={<Button text="right" />}
+      />
+
+      <Button
+        text="123"
+        type="DEFAULT"
+        onClick={() => {
+          console.log("123");
+        }}
+      />
+
+      <Button
+        text="123"
+        type="POSITIVE"
+        onClick={() => {
+          console.log("123");
+        }}
+      />
+
+      <Button
+        text="123"
+        type="NEGATIVE"
+        onClick={() => {
+          console.log("123");
+        }}
+      />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
-        <Route path="/diary" element={<Diary />} />
+        <Route path="/diary/:id" element={<Diary />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
